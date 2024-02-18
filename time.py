e9ba8f32a69f9bb0aeeba6b09b32d22972b2327c
Umme Fatema <ummefatema3333@gmail.com>
2024-02-18 18:32:15 +0900

    first commit

---
+++ time.py
@@ -0,0 +1,10 @@
+from datetime import date 
+import holidays 
+
+# Select country 
+uk_holidays = holidays.UnitedKingdom() 
+
+# Print all the holidays in UnitedKingdom in year 2018 
+for ptr in holidays.Japan(years = 2023).items(): 
+	print(ptr) 
+
